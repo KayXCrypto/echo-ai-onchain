161aa49d57b908b6dc03c8c7c0bac2c9981b43bf
KayXCrypto <nguyenmanhpro97@gmail.com>
2025-12-26 02:36:05 +0700

    v1.0.2

--- src/echo/index.ts
+++ src/echo/index.ts
@@ -1,5 +1,5 @@
 import Echo from '@merit-systems/echo-next-sdk';
-
+console.log(process.env.ECHO_APP_ID)
 export const { handlers, isSignedIn, openai, anthropic, google } = Echo({
   appId: process.env.ECHO_APP_ID!,
 });
--- src/providers.tsx
+++ src/providers.tsx
@@ -1,7 +1,8 @@
 'use client';
 
-import { EchoProvider } from '@merit-systems/echo-next-sdk/client';
+import { EchoProvider } from '@merit-systems/echo-react-sdk';
 
+console.log('ECHO APP ID:', process.env.NEXT_PUBLIC_ECHO_APP_ID);
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <EchoProvider config={{ appId: process.env.NEXT_PUBLIC_ECHO_APP_ID! }}>
